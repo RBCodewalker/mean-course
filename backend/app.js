1a2a9795f837252f950aac5c1b4a0460afa9bdea
Rajdeep Bastakoti <r7b@outlook.com>
2024-01-25 19:13:48 +0100

    Persistent posts feature added for each sesssion

--- backend/app.js
+++ backend/app.js
@@ -1,23 +1,64 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+const Post = require("./models/post");
 
 const app = express();
 
-app.use("/api/posts", (req, res, next) => {
-  const posts = [
-    {
-      id: "1",
-      title: "Server-side Post 1",
-      content: "This post is from the server!",
-    },
-    {
-      id: "2",
-      title: "Server-side Post 2",
-      content: "This post is from the server as well!",
-    },
-  ];
-  res.status(200).json({
-    message: "Posts fetched success!",
-    posts: posts,
+mongoose
+  .connect(
+    "mongodb+srv://rajdeep:ZR2XVB4ngcjumYG2@cluster0.ufd6pgu.mongodb.net/node-angular?retryWrites=true"
+  )
+  .then(() => {
+    console.log("Successfully connected to the database!");
+  })
+  .catch(() => {
+    console.log("Database connction failed!");
+  });
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PATCH, DELETE, OPTIONS"
+  );
+  next();
+});
+
+app.post("/api/posts", (req, res, next) => {
+  const post = new Post({
+    title: req.body.title,
+    content: req.body.content,
+  });
+  post.save().then(createdPost => {
+    res.status(201).json({
+      message: "Post has been successfully added.",
+      postId: createdPost._id,
+    });
+  });
+});
+
+app.get("/api/posts", (req, res, next) => {
+  Post.find().then((documents) => {
+    res.status(200).json({
+      message: "Posts fetched successfully!",
+      posts: documents,
+    });
+  });
+});
+
+app.delete("/api/posts/:id", (req, res, next) => {
+  Post.deleteOne({ _id: req.params.id }).then((result) => {
+    console.log(result);
+    res.status(200).json({ message: "Selected post is deleted!" });
   });
 });
 
--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Subject } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 import { Post } from './post.model';
 
@@ -13,9 +14,20 @@ export class PostsService {
 
   getPosts() {
     this.http
-      .get<{ message: string; posts: Post[] }>('http://localhost:3000/api/posts')
-      .subscribe((postData) => {
-        this.posts = postData.posts;
+      .get<{ message: string; posts: any }>('http://localhost:3000/api/posts')
+      .pipe(
+        map((postData: any) => {
+          return postData.posts.map((post: any) => {
+            return {
+              title: post.title,
+              content: post.content,
+              id: post._id,
+            };
+          });
+        })
+      )
+      .subscribe((changedPosts) => {
+        this.posts = changedPosts;
         this.postsUpdated.next([...this.posts]);
       });
     // return [...this.posts]; // the spread operator '...' helps to deep copy the array
@@ -27,7 +39,26 @@ export class PostsService {
 
   addPost(title: string, content: string) {
     const post: Post = { id: null, title: title, content: content };
-    this.posts.push(post);
-    this.postsUpdated.next([...this.posts]);
+    this.http
+      .post<{ message: string; postId: string }>(
+        'http://localhost:3000/api/posts',
+        post
+      )
+      .subscribe(responseData => {
+        const id = responseData.postId;
+        post.id = id;
+        this.posts.push(post);
+        this.postsUpdated.next([...this.posts]);
+      });
+  }
+
+  deletePost(postId: string) {
+    this.http
+      .delete('http://localhost:3000/api/posts/' + postId)
+      .subscribe(() => {
+        const updatedPosts = this.posts.filter((post) => post.id !== postId);
+        this.posts = updatedPosts;
+        this.postsUpdated.next([...this.posts]);
+      });
   }
 }
